8337c2a04ea55c663d898a2c970853c3b122e76f
jannikholsten <j.holsten@xsigns.de>
2021-11-23 16:09:18 +0100

    update 3.5.11 1

--- updates/update59.php
+++ updates/update59.php
@@ -15,153 +15,135 @@ class Update59 extends Migration
     {
         if (Schema::hasTable('xsigns_fewo_abrechnung'))
         {
-            Schema::table('xsigns_fewo_abrechnung', function ($table) {
-                $table->text('abr_memo')->default('')->change();
-                $table->text('abr_wartung')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_abrechnung " .
+                "modify column abr_memo text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column abr_wartung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_anglang'))
         {
-            Schema::table('xsigns_fewo_anglang', function ($table) {
-                $table->text('beschreibung')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_anglang " .
+                "modify column beschreibung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_bew'))
         {
-            Schema::table('xsigns_fewo_bew', function ($table) {
-                $table->text('nachricht')->default('')->change();
-                $table->text('antwort')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_bew " .
+                "modify column nachricht text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column antwort text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_buchung'))
         {
-            Schema::table('xsigns_fewo_buchung', function ($table) {
-                $table->text('leistungen')->default('')->change();
-                $table->text('mitreisende')->default('')->change();
-                $table->text('nachricht')->default('')->change();
-                $table->text('zuschlaege')->default('')->change();
-                $table->text('versicherung')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_buchung " .
+                "modify column leistungen text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column mitreisende text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column nachricht text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column zuschlaege text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column versicherung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_entflang'))
         {
-            Schema::table('xsigns_fewo_entflang', function ($table) {
-                $table->text('beschreibung')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_entflang " .
+                "modify column beschreibung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_halang'))
         {
-            Schema::table('xsigns_fewo_halang', function ($table) {
-                $table->text('beschreibung')->default('')->change();
-                $table->text('kurz')->default('')->change();
-                $table->text('seo')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_halang " .
+                "modify column beschreibung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column kurz text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column seo text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_images'))
         {
-            Schema::table('xsigns_fewo_images', function ($table) {
-                $table->text('description')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_images " .
+                "modify column description text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_lelang'))
         {
-            Schema::table('xsigns_fewo_lelang', function ($table) {
-                $table->text('beschreibung')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_lelang " .
+                "modify column beschreibung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_messages'))
         {
-            Schema::table('xsigns_fewo_messages', function ($table) {
-                $table->text('message')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_messages " .
+                "modify column message text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_objlang'))
         {
-            Schema::table('xsigns_fewo_objlang', function ($table) {
-                $table->text('beschreibung')->default('')->change();
-                $table->text('kurz')->default('')->change();
-                $table->text('seo')->default('')->change();
-                $table->text('lage')->default('')->change();
-                $table->text('anfrage')->default('')->change();
-                $table->text('preis1')->default('')->change();
-                $table->text('preis2')->default('')->change();
-                $table->text('baeder')->default('')->change();
-                $table->text('schlafzimmer')->default('')->change();
-                $table->text('html1')->default('')->change();
-                $table->text('html2')->default('')->change();
-                $table->text('html3')->default('')->change();
-                $table->text('innen')->default('')->change();
-                $table->text('aussen')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_objlang " .
+                "modify column beschreibung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column kurz text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column seo text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column lage text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column anfrage text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column preis1 text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column preis2 text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column baeder text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column schlafzimmer text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column html1 text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column html2 text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column html3 text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column innen text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column aussen text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_ralang'))
         {
-            Schema::table('xsigns_fewo_ralang', function ($table) {
-                $table->text('beschreibung')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_ralang " .
+                "modify column beschreibung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_raum'))
         {
-            Schema::table('xsigns_fewo_raum', function ($table) {
-                $table->text('ausst')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_raum " .
+                "modify column ausst text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_reglang'))
         {
-            Schema::table('xsigns_fewo_reglang', function ($table) {
-                $table->text('reglang_beschreibung')->default('')->change();
-                $table->text('reglang_kurz')->default('')->change();
-                $table->text('reglang_seo')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_reglang " .
+                "modify column reglang_beschreibung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column reglang_kurz text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column reglang_seo text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_setaus'))
         {
-            Schema::table('xsigns_fewo_setaus', function ($table) {
-                $table->text('ausstattung')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_setaus " .
+                "modify column ausstattung text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_slide_shows'))
         {
-            Schema::table('xsigns_fewo_slide_shows', function ($table) {
-                $table->text('slide_show_content')->default('')->change();
-                $table->text('responsive')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_slide_shows " .
+                "modify column slide_show_content text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column responsive text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_vorg'))
         {
-            Schema::table('xsigns_fewo_vorg', function ($table) {
-                $table->text('vorg_memo')->default('')->change();
-                $table->text('vorg_ehinweis')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_vorg " .
+                "modify column vorg_memo text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' ', " .
+                "modify column vorg_ehinweis text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_fewo_vorgleist'))
         {
-            Schema::table('xsigns_fewo_vorgleist', function ($table) {
-                $table->text('text')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_fewo_vorgleist " .
+                "modify column text text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
 
         if (Schema::hasTable('xsigns_weather_day'))
         {
-            Schema::table('xsigns_weather_day', function ($table) {
-                $table->text('description')->default('')->change();
-            });
+            Database::select(null, $this->modulename, "alter table xsigns_weather_day " .
+                "modify column description text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ' '");
         }
     }
 
